e850c0c440ee242f288d6cf921ea29d09cbdd769
NEGORO Tetsuya <tetsuya.negoro@thinkbiganalytics.com>
2019-01-29 17:10:35 +0900

    Fixed ConfigurableTask to work

--- task.py
+++ task.py
@@ -15,10 +15,6 @@ class ConfigurableTask(Configurable, Task, metaclass=MetaConfigurableTask):
     @classmethod
     def get_param_values(cls, params, args, kwargs):
         # Override the method of `Task` class not to pass `config` parameter.
-        try:
-            config = kwargs.pop('config', None)
-            return super().get_param_values(params, args, kwargs)
-        except:
-            raise
-        finally:
-            kwargs['config'] = config
+        kwargs = kwargs.copy()
+        kwargs.pop('config', None)
+        return super().get_param_values(params, args, kwargs)
